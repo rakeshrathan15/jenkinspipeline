8d3ea1eea6cef90774c8565b5a3a8b393bf8898f
rakesh mekala <rakeshyadavmekala15@gmail.com>
2024-10-27 09:57:31 +0530

    hello

--- src/main/java/com/neoteric/dockerlearning/DockerController.java
+++ src/main/java/com/neoteric/dockerlearning/DockerController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
+
+    int i= 10;
+
     @GetMapping("/dockerDemo")
     public String getHelloWorld() {
         return "Docker Demo with jenkins and docker" +
